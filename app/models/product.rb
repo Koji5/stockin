f7b9a900a2f3b3ce012f90c6c554bd506951a18a
Koji5 <76521976+Koji5@users.noreply.github.com>
2023-04-17 17:04:17 +0900

    バリデーションを追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,9 @@ class Product < ApplicationRecord
   has_many :supplier_products
   has_many :purchase_records
   has_many :suppliers, through: :supplier_products
+
+  validates :name, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :stock, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
 end
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -2,4 +2,8 @@ class Supplier < ApplicationRecord
   has_many :purchase_records
   has_many :supplier_products
   has_many :products, through: :supplier_products
+
+  validates :name, presence: true
+  validates :tel, presence: true
+
 end
